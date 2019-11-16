7326edd86aa16277122768b7168af3b5b54234de
Sadegh Ebrahimi <sadegh@stanford.edu>
2019-11-16 09:30:09 -0800

    Update CrossCorrelation.m

--- MatlabCodes/UsefulFunctions/CrossCorrelation.m
+++ MatlabCodes/UsefulFunctions/CrossCorrelation.m
@@ -1,3 +1,6 @@
+%%%%%Compute the Cross correlation between input x1 of 1 by L and x2 of 1 by L over the window of -T to T
+%%%%Inputs x1=1byL data, x2=1byL data, T scalar time window of -T to T for cross correlations
+%%%%output CC= 1by2T+1 cross correlation coefficient, MC= scalar maximum cross correlation coeeficient, MCind= scalar time shift for maximum cross correlations
 function [CC,MC,MCind]=CrossCorrelation(x1,x2,T)
 CC=zeros(1,(2*T)+1);
 L=length(x1);
